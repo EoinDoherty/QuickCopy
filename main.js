e74d03f98a371d5e03baec872ec28aeae5b0245e
Eoin Doherty <EoinDoherty@users.noreply.github.com>
2017-07-22 14:26:24 -0600

    Added menu for copy, paste, etc

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 var electron = require('electron');
 var BrowserWindow = electron.BrowserWindow;
+var Menu = require('menu');
 var app = electron.app;
 var ipc = electron.ipcMain;
 var win = null;
 
 app.on('ready', () => {
+
+  // Menu template
+  var template = [{
+    label: "QuickCopy",
+    submenu:[
+      {label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+      {label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:"},
+      {label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:"},
+      {label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:"}
+    ]
+  }];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
   win = new BrowserWindow({
     height: 500,
     width: 500
@@ -22,7 +37,7 @@ ipc.on('create-backup', (event, params) => {
   });
 
   progWin.loadURL('file://' + __dirname + '/app/progress-screen.html');
-  
+
   progWin.webContents.on('did-finish-load', () => {
     progWin.webContents.send('get-params', params);
   });
